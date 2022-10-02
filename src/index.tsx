ad7055aa53cc87230cb96524ba8bc5f3097dcfcc
yohan.wijaya <yohan.wijaya@shopee.com>
2022-10-03 00:36:51 +0700

    Feat(Configuration) Added FE Template & Protected Route

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <BrowserRouter>
+    <AuthProvider>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </AuthProvider>
+  </BrowserRouter>,
+);
