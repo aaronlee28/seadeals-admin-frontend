61a628d1b0669b1fb112a1fa0e8f060713683da9
rafly.nagachi <rafly.nagachi@shopee.com>
2022-10-07 13:27:28 +0700

    refactor(formatter): change class to function

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,13 +1,11 @@
-class Formatter {
-  static DisplayDatetime(time: string) {
-    const { length } = time;
-    return time.replace('T', ' ').substring(0, length - 4);
-  }
+const DisplayDatetime = (time: string) => {
+  const { length } = time;
+  return time.replace('T', ' ').substring(0, length - 4);
+};
 
-  static DisplayPrice(price: number) {
-    const priceSplit = price.toString().split(/(?=(?:\d{3})+(?:\.|$))/g);
-    return priceSplit.join('.');
-  }
-}
+const DisplayPrice = (price: number) => {
+  const priceSplit = price.toString().split(/(?=(?:\d{3})+(?:\.|$))/g);
+  return `Rp. ${priceSplit.join('.')}`;
+};
 
-export default Formatter;
+export default { DisplayDatetime, DisplayPrice };
