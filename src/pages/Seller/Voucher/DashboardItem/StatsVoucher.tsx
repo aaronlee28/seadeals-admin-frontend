121895a7dd41536bcfc769e8344703136acdf0df
rafly.nagachi <rafly.nagachi@shopee.com>
2022-10-10 11:01:08 +0700

    fix(voucher): styling voucher statistics

--- src/pages/Seller/Voucher/DashboardItem/StatsVoucher.tsx
+++ src/pages/Seller/Voucher/DashboardItem/StatsVoucher.tsx
@@ -25,13 +25,13 @@ const StatsVoucher = () =>
       <p className="mb-4 p-0">Statistik voucher toko untuk periode ...</p>
       <div className="row">
         <div className="col-4">
-          <CardStatistic title="Penjualan" data="Rp 40.000.000" />
+          <CardStatistic title="Penjualan" data="Rp 40.000.000" tooltip="Nilai total dari semua pesanan siap dikirim menggunakan Voucher yang ditanggung oleh Penjual, termasuk ongkos kirim dan tidak termasuk promosi lainnya, selama periode waktu yang dipilih." />
         </div>
         <div className="col-4">
-          <CardStatistic title="Pesanan" data="131" />
+          <CardStatistic title="Pesanan" data="131" tooltip="Jumlah voucher yang ditanggung Penjual yang digunakan pada pesanan selama jangka waktu tertentu." />
         </div>
         <div className="col-4">
-          <CardStatistic title="Pembeli" data="120" />
+          <CardStatistic title="Pembeli" data="120" tooltip="Jumlah Pembeli yang menggunakan setidaknya 1 voucher yang ditanggung Penjual pada pesanan selama jangka waktu tertentu." />
         </div>
       </div>
     </div>
