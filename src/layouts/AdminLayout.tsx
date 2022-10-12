54592ed7e8ab73c9aafe0a42d146f1424a8d8696
rafly.nagachi <rafly.nagachi@shopee.com>
2022-10-12 21:31:00 +0700

    feat(auth): useCheckLogin for seller and admin page

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar/Sidebar';
 import SidebarAdminData from './Sidebar/SidebarAdminData';
+import useCheckLogged from '../hooks/useCheckLogged';
 
-const AdminLayout = () => (
-  <div>
-    <Sidebar data={SidebarAdminData} />
-    <div className="main-content">
-      <Outlet />
+const AdminLayout = () => {
+  useCheckLogged();
+
+  return (
+    <div>
+      <Sidebar data={SidebarAdminData} />
+      <div className="main-content">
+        <Outlet />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AdminLayout;
--- src/layouts/SellerLayout.tsx
+++ src/layouts/SellerLayout.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar/Sidebar';
 import SidebarSellerData from './Sidebar/SidebarSellerData';
+import useCheckLogged from '../hooks/useCheckLogged';
 
-const SellerLayout = () => (
-  <div>
-    <Sidebar data={SidebarSellerData} />
-    <div className="">
-      <Outlet />
+const SellerLayout = () => {
+  useCheckLogged();
+  return (
+    <div>
+      <Sidebar data={SidebarSellerData} />
+      <div className="">
+        <Outlet />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SellerLayout;
