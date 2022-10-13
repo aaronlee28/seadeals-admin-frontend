18486f0494ddb35e7a05076f432a5e774359b47b
rafly.nagachi <rafly.nagachi@shopee.com>
2022-10-13 21:34:26 +0700

    feat(product): form product init

---
+++ src/pages/Seller/Product/FormItem/ProductOtherInfo.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+const ProductOtherInfo:FC<any> = ({ product, formType, handleOnChange }) => {
+  console.log(product);
+  console.log(formType);
+  console.log(handleOnChange);
+  return (
+    <div />
+  );
+};
+
+export default ProductOtherInfo;
---
+++ src/pages/Seller/Product/FormItem/ProductVariantInfo.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+const ProductVariantInfo:FC<any> = ({ product, formType, handleOnChange }) => {
+  console.log(product);
+  console.log(formType);
+  console.log(handleOnChange);
+  return (
+    <div />
+  );
+};
+
+export default ProductVariantInfo;
