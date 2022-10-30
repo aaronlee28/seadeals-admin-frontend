33dafa4db1c84334c17963796f2d6c289c0e3c02
rasyid.wijaya <rasyid.wijaya@shopee.com>
2022-10-30 20:15:48 +0700

    fix(create-promotions): correct product list fetching

--- src/pages/Seller/Promotion/PromotionFormItem/PromotionBonusInfo.tsx
+++ src/pages/Seller/Promotion/PromotionFormItem/PromotionBonusInfo.tsx
@@ -25,6 +25,7 @@ const PromotionBonusInfo:FC<any> = ({
 
   const [globalQuota, setGlobalQuota] = useState<any>(false);
 
+  const [tDFetch, setTDFetch] = useState<any>(false);
   const [tempData, setTempData] = useState<any>([]);
   const [unavailableProductsIds, setUnavailableProductsIds] = useState<any>([]);
   const [uPFetch, setUPFetch] = useState<any>(false);
@@ -33,12 +34,13 @@ const PromotionBonusInfo:FC<any> = ({
       .then((resp:any) => {
         const { data } = resp.data;
         setTempData(data);
+        setTDFetch(true);
       })
       .catch((err:any) => toast.error(err.response?.data?.message));
   }, []);
 
   useEffect(() => {
-    if (tempData.length > 0) {
+    if (tDFetch) {
       tempData.forEach((promo:any) => {
         if (promo.status === 'ongoing' || promo.status === 'upcoming') {
           setUnavailableProductsIds((prev:any) => [...prev, promo.product_id]);
@@ -46,7 +48,7 @@ const PromotionBonusInfo:FC<any> = ({
       });
       setUPFetch(true);
     }
-  }, [tempData, promotion.start_date, promotion.end_date]);
+  }, [tDFetch, promotion.start_date, promotion.end_date]);
 
   const findProducts = async () => {
     await ProductAPI.FindProductBySellerID(axiosPrivate, null)
@@ -122,45 +124,45 @@ const PromotionBonusInfo:FC<any> = ({
         <div className="col-9">
           <div className="row">
             {
-                          promotion.amount_type === VoucherConstant.NOMINAL
-                          && (
-                          <div className="input-group prefix p-0">
-                            <span className="input-group-addon">Rp</span>
-                            <input
-                              name="amount"
-                              className="form__input"
-                              placeholder="Masukkan angka"
-                              type="number"
-                              onChange={handleOnChange}
-                              required
-                              value={promotion.amount}
-                              readOnly={formType === VoucherConstant.SHOW}
-                              disabled={formType === VoucherConstant.SHOW}
-                            />
-                          </div>
-                          )
-                      }
+                  promotion.amount_type === VoucherConstant.NOMINAL
+                  && (
+                  <div className="input-group prefix p-0">
+                    <span className="input-group-addon">Rp</span>
+                    <input
+                      name="amount"
+                      className="form__input"
+                      placeholder="Masukkan angka"
+                      type="number"
+                      onChange={handleOnChange}
+                      required
+                      value={promotion.amount}
+                      readOnly={formType === VoucherConstant.SHOW}
+                      disabled={formType === VoucherConstant.SHOW}
+                    />
+                  </div>
+                  )
+              }
             {
-                          promotion.amount_type === VoucherConstant.PERCENTAGE
-                          && (
-                          <div className="input-group suffix p-0">
-                            <input
-                              name="amount"
-                              min={0}
-                              max={100}
-                              className="form__input"
-                              placeholder="Masukkan angka"
-                              type="number"
-                              onChange={handleOnChange}
-                              required
-                              value={promotion.amount}
-                              readOnly={formType === VoucherConstant.SHOW}
-                              disabled={formType === VoucherConstant.SHOW}
-                            />
-                            <span className="input-group-addon">% (Persen)</span>
-                          </div>
-                          )
-                      }
+                  promotion.amount_type === VoucherConstant.PERCENTAGE
+                  && (
+                  <div className="input-group suffix p-0">
+                    <input
+                      name="amount"
+                      min={0}
+                      max={100}
+                      className="form__input"
+                      placeholder="Masukkan angka"
+                      type="number"
+                      onChange={handleOnChange}
+                      required
+                      value={promotion.amount}
+                      readOnly={formType === VoucherConstant.SHOW}
+                      disabled={formType === VoucherConstant.SHOW}
+                    />
+                    <span className="input-group-addon">% (Persen)</span>
+                  </div>
+                  )
+              }
           </div>
         </div>
       </div>
@@ -212,79 +214,79 @@ const PromotionBonusInfo:FC<any> = ({
         </div>
       </div>
       {
-        promotion.amount > 0 && (
-        <>
-          <h5 className="text-start"><b>Pengaturan Produk</b></h5>
-          <div className="row my-3">
-            <label className="col-3 text-end align-self-center" htmlFor="promosi-per-produk">Produk</label>
-            <div className="col-9">
-              <Button buttonType="secondary alt" text="Tambah" handleClickedButton={() => setShow(true)} />
-              {
-                            show && (
-                            <Modal modalType="" cancel={() => setShow(false)}>
-                              <div className="d-flex py-5 justify-content-center">
-                                <div className="row px-5">
-                                  <h5 className="text-start mb-4"><b>Tambah promosi per produk</b></h5>
-                                  <div className="row mt-2 mb-5">
-                                    <label className="col-3 text-end align-self-center mb-3" htmlFor="product">Pilih produk</label>
-                                    <div className="col-9 p-0 mb-3">
-                                      <select className="form-select my-auto" onChange={handleSetCurrentProduct} defaultValue="choose product">
-                                        <option value="choose product" disabled>Pilih produk</option>
-                                        {
-                                                            products.map(
-                                                              (
-                                                                product:any,
-                                                              ) => (
-                                                                <option
-                                                                  key={product.id}
-                                                                  value={product.id}
-                                                                >
-                                                                  {product.name}
-                                                                </option>
-                                                              ),
-                                                            )
-                                        }
-                                      </select>
-                                    </div>
-                                    <label className="col-3 text-end align-self-center mb-3" htmlFor="product">Kuota Promosi</label>
-                                    <div className="col-9 p-0 mb-3">
-                                      <input className="form-control" placeholder="Masukkan jumlah kuota promosi" onChange={(e) => setQuota(e.target.value)} />
-                                    </div>
-                                    <label className="col-3 text-end align-self-center" htmlFor="product">Jumlah Maksimal</label>
-                                    <div className="col-9 p-0">
-                                      <input className="form-control" placeholder="Masukkan jumlah maksimal" onChange={(e) => setMaxQuota(e.target.value)} />
-                                    </div>
+            promotion.amount > 0 && (
+            <>
+              <h5 className="text-start"><b>Pengaturan Produk</b></h5>
+              <div className="row my-3">
+                <label className="col-3 text-end align-self-center" htmlFor="promosi-per-produk">Produk</label>
+                <div className="col-9">
+                  <Button buttonType="secondary alt" text="Tambah" handleClickedButton={() => setShow(true)} />
+                  {
+                          show && (
+                          <Modal modalType="" cancel={() => setShow(false)}>
+                            <div className="d-flex py-5 justify-content-center">
+                              <div className="row px-5">
+                                <h5 className="text-start mb-4"><b>Tambah promosi per produk</b></h5>
+                                <div className="row mt-2 mb-5">
+                                  <label className="col-3 text-end align-self-center mb-3" htmlFor="product">Pilih produk</label>
+                                  <div className="col-9 p-0 mb-3">
+                                    <select className="form-select my-auto" onChange={handleSetCurrentProduct} defaultValue="choose product">
+                                      <option value="choose product" disabled>Pilih produk</option>
+                                      {
+                                            products.map(
+                                              (
+                                                product:any,
+                                              ) => (
+                                                <option
+                                                  key={product.id}
+                                                  value={product.id}
+                                                >
+                                                  {product.name}
+                                                </option>
+                                              ),
+                                            )
+                                          }
+                                    </select>
                                   </div>
-                                  <div className="d-inline-flex justify-content-end gap-3">
-                                    <Button buttonType="secondary alt" text="Tutup" handleClickedButton={() => setShow(false)} />
-                                    <Button buttonType="primary" text="Tambah" handleClickedButton={handleAddProduct} />
+                                  <label className="col-3 text-end align-self-center mb-3" htmlFor="product">Kuota Promosi</label>
+                                  <div className="col-9 p-0 mb-3">
+                                    <input className="form-control" placeholder="Masukkan jumlah kuota promosi" onChange={(e) => setQuota(e.target.value)} />
+                                  </div>
+                                  <label className="col-3 text-end align-self-center" htmlFor="product">Jumlah Maksimal</label>
+                                  <div className="col-9 p-0">
+                                    <input className="form-control" placeholder="Masukkan jumlah maksimal" onChange={(e) => setMaxQuota(e.target.value)} />
                                   </div>
                                 </div>
+                                <div className="d-inline-flex justify-content-end gap-3">
+                                  <Button buttonType="secondary alt" text="Tutup" handleClickedButton={() => setShow(false)} />
+                                  <Button buttonType="primary" text="Tambah" handleClickedButton={handleAddProduct} />
+                                </div>
                               </div>
-                            </Modal>
-                            )
-                        }
-            </div>
-          </div>
-          <ProductListInfo
-            addedProduct={addedProduct}
-            products={products}
-            promotion={promotion}
-            setPromotions={setPromotions}
-            setDeletedID={setDeletedID}
-            discount={promotion.amount}
-            quota={quota === '' ? promotion.quota : quota}
-            globalQuota={globalQuota}
-            setGlobalQuota={setGlobalQuota}
-            globalQuotaValue={quota === '' ? promotion.quota : quota}
-            globalMaxQuotaValue={maxQuota === '' ? promotion.max_quota : maxQuota}
-            setQuota={setQuota}
-            maxQuota={maxQuota === '' ? promotion.max_quota : maxQuota}
-            setMaxQuota={setMaxQuota}
-            promotionType={promotion.amount_type}
-          />
-        </>
-        )
+                            </div>
+                          </Modal>
+                          )
+                      }
+                </div>
+              </div>
+              <ProductListInfo
+                addedProduct={addedProduct}
+                products={products}
+                promotion={promotion}
+                setPromotions={setPromotions}
+                setDeletedID={setDeletedID}
+                discount={promotion.amount}
+                quota={quota === '' ? promotion.quota : quota}
+                globalQuota={globalQuota}
+                setGlobalQuota={setGlobalQuota}
+                globalQuotaValue={quota === '' ? promotion.quota : quota}
+                globalMaxQuotaValue={maxQuota === '' ? promotion.max_quota : maxQuota}
+                setQuota={setQuota}
+                maxQuota={maxQuota === '' ? promotion.max_quota : maxQuota}
+                setMaxQuota={setMaxQuota}
+                promotionType={promotion.amount_type}
+              />
+            </>
+            )
         }
     </div>
   );
